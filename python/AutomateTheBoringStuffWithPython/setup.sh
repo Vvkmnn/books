1f38fb8ef65278a08e6e859d679899940ae77a01
Vivek Menon <vvk.mnn@gmail.com>
2016-01-20 10:32:44 -0500

    Quick setup.sh edit
    Changed lesson1.sh to setup.sh

--- python/AutomateTheBoringStuffWithPython/setup.sh
+++ python/AutomateTheBoringStuffWithPython/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# install and setup brew and cask for applications
+# For OSX users: install and setup brew and cask for code and applications
+# For UNIX users: use whatever package manager you typically use to get these packages. 
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
