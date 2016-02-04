09ae62a2dfb5f681620bad275c5be0e4a7a421c9
Vivek Menon <vvk.mnn@gmail.com>
2016-02-04 16:27:41 -0500

    Lesson 0 & Housekeeping
    Introduction and file cleanup.

--- python/AutomateTheBoringStuffWithPython/template.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Lecture NA: Functions
-"""Lecture NA of "Automate the Boring Stuff with Python."""
-__author__ = 'Vivek Menon'
-
-
-# Recap:
-# - You can import modules and get access to new functions.
